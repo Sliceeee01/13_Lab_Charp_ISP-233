3cb409a5635c3a957aed5343bced961b65f9cefd
Sliceeee01 <ivan_hih@icloud.com>
2025-10-27 15:09:09 +0300

    Add project files.

---
+++ 13_Lab_Charp_ISP-233/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _13_Lab_Charp_ISP_233;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
